5527ee52aed7316bd719deb6b89e91d6bd1f6170
codeSpiders <102961067+codeSpiders@users.noreply.github.com>
2022-04-23 10:37:46 +0800

    main函数的参数

---
+++ ArgsTest.java
@@ -0,0 +1,10 @@
+package cn.hengyan.day24;
+
+public class ArgsTest {
+	public static void main(String[] args) {
+		System.out.println(args);
+		System.out.println(args.length);
+		for(int i=0;i<args.length;i++)
+			System.out.println(args[i]);
+	}
+}
