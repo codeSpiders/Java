6b7f7b80387537d7d664a39260e137633c728324
codeSpiders <102961067+codeSpiders@users.noreply.github.com>
2022-05-29 11:09:56 +0800

    44MathTest

---
+++ MathTest.java
@@ -0,0 +1,16 @@
+package cn.hengyan.day44;
+
+public class MathTest {
+	public static void main(String[] args) {
+		System.out.println(Math.E);
+		System.out.println(Math.PI);
+		System.out.println(Math.round(Math.PI));
+		System.out.println(Math.ceil(3.56));
+		System.out.println(Math.floor(5.61));
+		System.out.println(Math.pow(2.6,3.5));
+		System.out.println(Math.max(5.6,6.3));
+		System.out.println(Math.min(6.54, 35.1));
+		System.out.println(Math.random()); 
+		
+	}
+}
